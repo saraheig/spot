3e112a0c6316499910be5fbe42a057003a29eab2
saraheig <sarah.composto@heig-vd.ch>
2019-04-05 21:45:05 +0200

    Set up Active Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AdminUser.delete_all
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
