bcc674e4f5d63e5559d7598e577ab7852bcf9397
saraheig <sarah.composto@heig-vd.ch>
2019-04-10 16:39:20 +0200

    Add Place seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,36 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 AdminUser.delete_all
+Place.delete_all
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(
+  email: 'admin@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+) if Rails.env.development?
+
+Place.create!([{
+  title: 'Le Double R',
+  description: 'Bar & grill à Yverdon-les-Bains, le Double R propose de la cuisine américaine, des hamburgers. Végétarien-ne-s également les bienvenu-e-s !',
+  price: 25.00,
+  duration: 90,
+  schedule: 'Heures d\'ouverture : 09:00-23:00',
+  lat: 46.779951,
+  lng: 6.637833
+},{
+  title: 'La Maison d\'Ailleurs',
+  description: 'Ce musée de la science-fiction, de l\'utopie et des voyages extraordinaires propose diverses expositions tout au lond de l\'année.',
+  price: 12.00,
+  duration: 120,
+  schedule: 'Ma-di : 11:00-18:00',
+  lat: 46.778594,
+  lng: 6.641896
+},{
+  title: 'Numerik Games Festival',
+  description: 'Le Numerik Games Festival est dédié à l\'art et la culture numériques.',
+  price: 30.00,
+  duration: 180,
+  schedule: 'Dates pour 2019 : 30 août au 1er septembre',
+  lat: 46.766403,
+  lng: 6.645001
+},])
