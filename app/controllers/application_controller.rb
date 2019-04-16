ae77d6278996783158ce16434e318923e747c151
saraheig <sarah.composto@heig-vd.ch>
2019-04-16 11:50:25 +0200

    Modify i18n configuration (thx to thibaudgg)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
-  def set_locale
-    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-    if (I18n.available_locales.any?{|loc| loc.to_s == extract_locale_from_accept_language_header})
-      # Get the language from the browser only if it is in the available_locales of the app
-      I18n.locale = extract_locale_from_accept_language_header
-    end
-    logger.debug "* Locale set to '#{I18n.locale}'"
-  end
-
   private
-    def extract_locale_from_accept_language_header
-      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    def set_locale
+      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+      # Get the language of the browser using the safe navigation operator
+      header_locale = request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first&.to_sym
+      if I18n.available_locales.include? header_locale
+        # Get the language of the browser only if it is in the available_locales of the app
+        I18n.locale = header_locale
+      end
+      logger.debug "* Locale set to '#{I18n.locale}'"
     end
 end
