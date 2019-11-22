d03460e078034074d3555b7f46ff156b2a6fee8d
saraheig <sarah.composto@heig-vd.ch>
2019-11-22 21:03:16 +0100

    Add models tests and system tests

--- test/controllers/places_controller_test.rb
+++ test/controllers/places_controller_test.rb
@@ -17,8 +17,9 @@ class PlacesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create place' do
     assert_difference('Place.count') do
+      img = fixture_file_upload('files/rails.jpg', 'application/jpg')
       post places_url, params: { place: {
-        title: 'NewPlace', description: @place.description, price_chf: @place.price_chf, duration_minutes: @place.duration_minutes, schedule: @place.schedule, lat: @place.lat, lng: @place.lng
+        title: 'NewPlace', image: img, description: @place.description, price_chf: @place.price_chf, duration_minutes: @place.duration_minutes, schedule: @place.schedule, lat: @place.lat, lng: @place.lng
       } }
     end
 
