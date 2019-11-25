f0005fb3349d500b83812a0fb9deee9a3b49fd0b
saraheig <sarah.composto@heig-vd.ch>
2019-11-25 14:36:49 +0100

    .

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,21 +2,20 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'coveralls'
-require 'selenium/webdriver'
+# require 'selenium/webdriver'
 Coveralls.wear!
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu disable-dev-shm-usage])
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-Capybara.javascript_driver = :headless_chrome # or optional :chrome for getting a browser locally
-
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+#
+# Capybara.register_driver :headless_chrome do |app|
+#   options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu disable-dev-shm-usage])
+#
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
+#
+# Capybara.javascript_driver = :headless_chrome # or optional :chrome for getting a browser locally
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
